f234c366e506f08c8d97d75919ce950238641165
Faiza Eba Batool <faizabatool1691@gmail.com>
2021-11-19 12:35:35 -0800

    cors and api controllers added

--- biddr_api/biddr_api/config/routes.rb
+++ biddr_api/biddr_api/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+
+    namespace :v1 do
+      resources :auctions
+        resource :session, only: [:create, :destroy]
+      #   get('users/current',{to: 'users#current'})
+            resources :users, only: [:create] do
+                  #get :current -> api/v1/users/:user_id/current
+                  get :current, on: :collection # -> api/v1/users/current
+            end
+    end
+  end
 end
